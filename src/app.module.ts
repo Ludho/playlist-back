c739d0f87d13c33309bb3da647897f6c240cf589
Ludho <Ludho234@gmail.com>
2022-12-28 23:08:42 +0100

    🔨 add env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,17 @@ import { UsersModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { VideosModule } from './videos/videos.module';
 import { YoutubeService } from './youtube/youtube.service';
+require('dotenv').config()
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: '',
-      database: 'yourtube',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       charset: 'utf8mb4'
